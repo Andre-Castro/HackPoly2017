34629b7b527babacc430286751f1cb9fa8c40436
smeno004 <siddharth.krishnaleela@gmail.com>
2017-02-11 16:09:07 -0800

    EdgeDetectionTests
    Edges man, edges! and Feature Detection!

--- HackPoly.py
+++ HackPoly.py
@@ -1,5 +1,6 @@
 import numpy
 import cv2
+from matplotlib import pyplot as plt
 
 #img = cv2.imread('d:\SidSpace\Laptop.png',1)
 
@@ -9,12 +10,23 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-while (True):
+orb = cv2.ORB_create()
+
+while (cap.isOpened()):
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
 
-    cv2.imshow('frame', gray)
+    kp = orb.detect(frame, None)
+
+    kp, des = orb.compute(frame, kp)
+
+    frame2 = cv2.drawKeypoints(frame, kp, None, color=(0,255,0), flags=0)
+
+    cv2.imshow('frame', frame2)
+    
+    #plt.imshow(frame2),plt.show()
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
