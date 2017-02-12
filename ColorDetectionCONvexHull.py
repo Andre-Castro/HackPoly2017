24d6dfbf9f2e4ce8e872a5da0abe4a812904ab64
smeno004 <siddharth.krishnaleela@gmail.com>
2017-02-12 11:12:52 -0800

    Final HackPoly commit?

--- ColorDetectionCONvexHull.py
+++ ColorDetectionCONvexHull.py
@@ -9,15 +9,20 @@ import time
 def nothing(x):
     pass
 
+#Initializes the GUI for drawing screen
 master = Tk()
 
+#Initializes Camera Capture
 cap = cv2.VideoCapture(0)
-#cap = cv2.VideoCapture('Andre2999.mp4')
+
+#Initializes windows
 cv2.namedWindow('cap')
 cv2.namedWindow('mask')
 
+#A Pause for calibration
 calibrationPause = 20
 
+#For sliders to test masking (Uncomment, for recalibration)
 lb = 0
 lg = 0
 lr = 0
@@ -28,8 +33,10 @@ ur = 0
 
 sigma = 0.0
 
+#Initialize ORB feature detector
 orb = cv2.ORB_create(nfeatures=225, edgeThreshold=32, WTA_K=3, patchSize=32)
 
+#Initialize variables for drawing calculations
 applicableContours = []
 
 initial_list = []
@@ -44,12 +51,15 @@ avg_xSum = 0
 
 reqx = 800
 
+#Initialize Canvas Parameters
 canvas_width = 800
 canvas_height = 600
 
+#Initialize Canvas
 w = Canvas(master, width=canvas_width, height=canvas_height)
 w.pack()
 
+#More variables
 currXPos = 0
 currYPos = 0
 
@@ -62,6 +72,7 @@ currYPos = 0
 ##turtle.pendown()
 
 # create trackbars for color change
+#(Uncomment for mask recalibration)
 cv2.createTrackbar('loR','cap',0,255,nothing)
 cv2.createTrackbar('loG','cap',0,255,nothing)
 cv2.createTrackbar('loB','cap',0,255,nothing)
@@ -76,22 +87,29 @@ cv2.createTrackbar('upB','cap',0,255,nothing)
 switch = '0 : OFF \n1 : ON'
 cv2.createTrackbar(switch, 'cap',0,1,nothing)
 
+#Start analyzing video capture and drawing
 while(cap.isOpened()):
-    max_y = 600
-    
-    ret,frame = cap.read()
-    frame = cv2.resize(frame, (800, 600))
-    
-    lower_pink = np.array([56,35,121], dtype = "uint8")
-    upper_pink = np.array([125,93,145], dtype = "uint8")
+    max_y = 600 #Sets lowest point of canvas
 
+    #Read each frame of capture
+    ret,frame = cap.read()
+    #Resize Frame
+    frame = cv2.resize(frame, (800, 600))
+
+    #Color range for glove mask
+    lower_pink = np.array([56,37,94], dtype = "uint8")
+    upper_pink = np.array([95,66,145], dtype = "uint8")
+
+    #Apply Gaussian Blur to reduce noise
     frame = cv2.GaussianBlur(frame, (5,5),3)
     #cv2.imshow('frameBlur',frame)
 
+    #Convert frame to enhance mask
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2Lab)
 
     framesList = cv2.split(frame)
 
+    #Apply CLAHE
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     framesList[0] = clahe.apply(framesList[0])
 
@@ -99,15 +117,21 @@ while(cap.isOpened()):
 
     frame = cv2.cvtColor(frame, cv2.COLOR_Lab2BGR)
 
+    #Set Mask
     mask = cv2.inRange(frame,lower_pink,upper_pink)
-    
+
+    #Shows Mask
+    #(Comment if unnecessary)
     cv2.imshow('mask', mask)
     #cv2.imshow('frame', frame)
+
+    #(If 'escape' input, exit)
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
 
     # get current positions of four trackbars
+    #(Uncomment for mask calibration)
     lr = cv2.getTrackbarPos('loR','cap')
     lg = cv2.getTrackbarPos('loG','cap')
     lb = cv2.getTrackbarPos('loB','cap')
@@ -123,14 +147,18 @@ while(cap.isOpened()):
     #frame[:] = [ub,ug,ur]
 
     #cv2.imshow('frame', frame)
-    
+
     res = cv2.bitwise_and(frame,frame,mask= mask)
 
+    #Detect keypoints in the frame
     kp = orb.detect(res,None)
+    #Compute descriptors
     kp, des = orb.compute(res, kp)
 
+    #Draw keypoints onto frame
     frame = cv2.drawKeypoints(frame, kp, None, color=(0,255,0), flags=0)
 
+    #Find contours and draw them
     res = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
     im2, contours, hierarachy = cv2.findContours(res, cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
     frame = cv2.drawContours(frame, contours, -1, (255,0,0), 3)
@@ -138,21 +166,27 @@ while(cap.isOpened()):
     #print(contours[0][0][0][0])
     #print(contours[0].shape)
 
-
-
+    #If this is a dead frame, skip
     if frame is None:
         continue
 
+    #Frame2 is None, only if target glove is not on screen
     if frame2 is None:
+        #Calibration Pause
         if calibrationPause == 0:
             pass
         else:
             calibrationPause = calibrationPause - 1
+
+        #Check contours against an area threshold
+        #Add to applicableContours only if area is greater than threshold
         for currContour in range(len(contours)):
             area = cv2.contourArea(contours[currContour])
             if area > 350:
                 applicableContours.append(currContour)
                 gloveDetect = True
+
+        #If glove is detected, set initial position of target
         if gloveDetect is True:            
             for specContour in applicableContours:
                 extTop = tuple(contours[specContour][contours[specContour][:, :, 1].argmin()][0])
@@ -179,13 +213,20 @@ while(cap.isOpened()):
     #    hull.append(cv2.convexHull(element))
     #frame = cv2.drawContours(frame, hull, -1, (255,255,0), 3)
 
+    #If glove has been detected in previous frame, track motion to draw
     if frame2 is not None:
+        #If glove moves out of screen, program must detect it again
         gloveDetect = False
+
+        #Check against area threshold again to check if glove is still in frame
         for currContour in range(len(contours)):
             area = cv2.contourArea(contours[currContour])
             if area > 350:
                 applicableContours.append(currContour)
                 gloveDetect = True
+
+        #Find position of next position
+        #Uses averages of two consecutive frames for a little more precision
         if gloveDetect is True:
             for specContour in applicableContours:
                 extTop = tuple(contours[specContour][contours[specContour][:, :, 1].argmin()][0])
@@ -200,6 +241,8 @@ while(cap.isOpened()):
                 
 ##                turtle.sety((300-avg_ySum))
 ##                turtle.setx(-(-400+avg_xSum))
+
+                #draw on the pixel on screen (given by coordinates)
                 w.create_line(currXPos, currYPos, canvas_width - avg_xSum, avg_ySum, fill="#476042")
                 currXPos = canvas_width - avg_xSum
                 currYPos = avg_ySum
